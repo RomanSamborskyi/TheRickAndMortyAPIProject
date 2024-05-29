6f955e3a972bdd02da202d7dc0350256be68c475
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-05-29 20:20:14 +0200

    refactoring fetching methods in episodes view model

--- SomePrsctice/Core/Episodes/ViewModel/EpisodesViewModel.swift
+++ SomePrsctice/Core/Episodes/ViewModel/EpisodesViewModel.swift
@@ -26,36 +26,57 @@ class EpisodesViewModel: ObservableObject {
     
     ///Func to get episodes for specific character
     func getEpisodes(for character: Character) async throws {
-        var episodes: [Episode] = []
         
-        for episodeURL in character.episode {
-            guard let url = URL(string: episodeURL) else {
-                throw AppError.badURL
-            }
+        let result = try await withThrowingTaskGroup(of: Episode.self) { group in
+            var episodes: [Episode] = []
             
-            if let episode = try await apiManager.download(with: url, type: Episode.self) {
-                episodes.append(episode)
+            for episodeURL in character.episode {
+                guard let url = URL(string: episodeURL) else {
+                    throw AppError.badURL
+                }
+                
+                group.addTask {
+                    guard let episode = try await self.apiManager.download(with: url, type: Episode.self) else {
+                        throw URLError(.dataNotAllowed)
+                    }
+                    return episode
+                }
+                for try await item in group {
+                    episodes.append(item)
+                }
             }
+            return episodes
         }
-        
-        DispatchQueue.main.async {
-            self.episodesForCharacter[character] = episodes
+        await MainActor.run {
+            self.episodesForCharacter[character] = result
         }
     }
     ///Func to get characters fot current episode
     func getExtraInfo(for episode: Episode) async throws {
-        var array:[Character] = []
-        for url in episode.characters {
-            guard let url = URL(string: url) else {
-                throw AppError.badURL
-            }
+
+        let result = try await withThrowingTaskGroup(of: Character.self) { group in
+            var characters: [Character] = []
             
-            if let character = try await apiManager.download(with: url, type: Character.self) {
-                array.append(character)
+            for url in episode.characters {
+                guard let url = URL(string: url) else {
+                    throw AppError.badURL
+                }
+                
+                group.addTask {
+                    guard let character = try await self.apiManager.download(with: url, type: Character.self) else {
+                        throw URLError(.dataNotAllowed)
+                    }
+                    return character
+                }
+                
+                for try await item in group {
+                    characters.append(item)
+                }
             }
+            return characters
         }
-        DispatchQueue.main.async {
-            self.characters[episode] = array
+        await MainActor.run {
+            self.characters[episode] = result
         }
     }
     ///Func to get episodes list
